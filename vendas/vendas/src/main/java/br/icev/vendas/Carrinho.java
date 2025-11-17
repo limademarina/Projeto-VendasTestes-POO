c3af8e2c4122544ddc1f77623ad9bfb86ce44ad5
limademarina <marina.fonseca@somosicev.com>
2025-11-17 15:11:25 -0300

    Criação da classe Carrinho.

--- vendas/vendas/src/main/java/br/icev/vendas/Carrinho.java
+++ vendas/vendas/src/main/java/br/icev/vendas/Carrinho.java
@@ -1,22 +1,60 @@
 package br.icev.vendas;
 
 import br.icev.vendas.excecoes.QuantidadeInvalidaException;
+
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Carrinho {
+
+    private List<ItemCarrinho> itens = new ArrayList<>();
+
     public void adicionar(Produto produto, int quantidade) throws QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Deve ser maior que zero.");
+        }
+        if (produto == null) {
+            throw new IllegalArgumentException("Não pode ser nulo.");
+        }
+
+        for (ItemCarrinho item : itens) {
+            if (item.getProduto().equals(produto)) {
+                item.setQuantidade(item.getQuantidade() + quantidade);
+                return;
+            }
+        }
+
+        ItemCarrinho novoItem = new ItemCarrinho(produto, quantidade);
+        itens.add(novoItem);
     }
 
     public BigDecimal getSubtotal() {
-        throw new UnsupportedOperationException("TODO");
-    }
+        BigDecimal subtotal = BigDecimal.ZERO;
 
-    public BigDecimal getTotalCom(PoliticaDesconto politica) {
-        throw new UnsupportedOperationException("TODO");
+        for (ItemCarrinho item : itens) {
+            subtotal = subtotal.add(item.getValorTotal());
+        }
+
+        return subtotal;
     }
 
     public int getTotalItens() {
-        throw new UnsupportedOperationException("TODO");
+        int totalUnidades = 0;
+
+        for (ItemCarrinho item : itens) {
+            totalUnidades += item.getQuantidade();
+        }
+
+        return totalUnidades;
     }
-}
+
+    public BigDecimal getTotalCom(PoliticaDesconto politica) {
+        if (politica == null) {
+            throw new IllegalArgumentException("Não pode ser nula.");
+        }
+
+        BigDecimal subtotal = getSubtotal();
+        return politica.aplicarDesconto(subtotal);
+    }
+}}
