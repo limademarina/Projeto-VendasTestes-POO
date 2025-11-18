51e21ee25af3ceb911281d5ff6376d3bf134b5e2
limademarina <marina.fonseca@somosicev.com>
2025-11-17 22:28:43 -0300

    Criação da classe Produto. Produto Teste.

--- vendas/vendas/src/main/java/br/icev/vendas/Produto.java
+++ vendas/vendas/src/main/java/br/icev/vendas/Produto.java
@@ -1,24 +1,50 @@
 package br.icev.vendas;
 
+import br.icev.vendas.excecoes.QuantidadeInvalidaException;
+
 import java.math.BigDecimal;
 
 public class Produto {
-    private final String codigo;
-    private final String nome;
-    private final BigDecimal precoUnitario;
+    private String codigo;
+    private String nome;
+    private BigDecimal precoUnitario;
 
     public Produto(String codigo, String nome, BigDecimal precoUnitario) {
+
+       if (precoUnitario == null) {
+           throw new NullPointerException("Preço não pode ser nulo!");
+       }
+       if (precoUnitario.compareTo(BigDecimal.ZERO) <= 0) {
+           throw new IllegalArgumentException("Preço deve ser maior que zero");
+       }
+
         this.codigo = codigo;
         this.nome = nome;
         this.precoUnitario = precoUnitario;
     }
 
-    public String getCodigo() { throw new UnsupportedOperationException("TODO"); }
-    public String getNome() { throw new UnsupportedOperationException("TODO"); }
-    public BigDecimal getPrecoUnitario() { throw new UnsupportedOperationException("TODO"); }
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
 
     @Override
-    public boolean equals(Object o) { throw new UnsupportedOperationException("TODO"); }
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return codigo != null && codigo.equals(produto.codigo);
+    }
+
     @Override
-    public int hashCode() { throw new UnsupportedOperationException("TODO"); }
+    public int hashCode() {
+        return (codigo == null) ? 0 : codigo.hashCode();
+    }
 }
